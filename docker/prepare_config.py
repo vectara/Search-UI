9c960c557415b531ddd969c52e0d364a74dc065e
Tomas Laurinavicius <hi@tomaslau.com>
2023-05-06 17:50:42 +0200

    changes

--- docker/prepare_config.py
+++ docker/prepare_config.py
@@ -2,20 +2,21 @@
 import sys
 import yaml
 
+
 def main(filename):
-    
     config = yaml.load(open(filename), Loader=yaml.FullLoader)
-    config['endpoint'] = 'api.vectara.io'
+    config["endpoint"] = "api.vectara.io"
 
     # convert serving attributes to have REACT_APP prefix
     new_config = {}
-    for k,v in config.items():
-        new_config['REACT_APP_' + k] = v
+    for k, v in config.items():
+        new_config["REACT_APP_" + k] = v
 
-    with open('.env', 'w') as f:
+    with open(".env", "w") as f:
         for k in new_config.keys():
-            f.write(f'{k}={new_config[k]}\n')
+            f.write(f"{k}={new_config[k]}\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     filename = sys.argv[1]
     main(filename)
